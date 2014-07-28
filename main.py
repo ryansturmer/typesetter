98c507ec796cdc275ead57dd741c0561607ade2b
Ryan Sturmer <ryansturmer@gmail.com>
2014-07-28 13:49:27 -0400

    Update to produce fancy graph

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ import numpy as np
 from skimage import io, exposure
 import graph
 
-toolpath = typesetter.typeset('Hello, World!', 'Vera.ttf')
 
-#zees = [[size_map[x] for x in path] for path in paths]
+def plot():
+    import matplotlib.pyplot as plt
+    from mpl_toolkits.mplot3d import Axes3D
 
-#import matplotlib.pyplot as plt
-#from mpl_toolkits.mplot3d import Axes3D
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.set_autoscalez_on(False)
+    ax.set_zlim([-500.0,0.0])
+    toolpath.scale(z=-1.0)
+    for path in toolpath:
+        x,y,z = zip(*path)
+        ax.plot(x,y,z)
 
-#fig = plt.figure()
-#ax = fig.add_subplot(111, projection='3d')
+    plt.show()
 
-#for path, z in zip(paths, zees):
-#    x,y = zip(*path)
-#    ax.plot(x,y,z)
-
-#plt.show()
+if __name__ == "__main__":
+    toolpath = typesetter.typeset('Hello, World!', 'Vera.ttf')
+    plot()
--- mmorph.py
+++ mmorph.py
@@ -1,5 +1,5 @@
 from numpy import array, maximum, int8, zeros_like
-from scipy.ndimage.morphology import generate_binary_structure, iterate_structure, binary_dilation, binary_erosion, binary_opening
+from scipy.ndimage.morphology import generate_binary_structure, iterate_structure, binary_dilation, binary_erosion, binary_opening, distance_transform_edt
 
 image = array(
     [[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
@@ -14,26 +14,12 @@ image = array(
     [0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0]],
+    [0,0,0,0,0,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]],
     dtype=int8
     )
 
-cross = generate_binary_structure(2, 1)
-print cross
-def mmskelm(f,B=cross):
-    y = zeros_like(f)
-    for i in range(max(f.shape)+1):
 
-        # Dilate the cross structure with itself (i) times
-        nb = iterate_structure(cross, i)
-
-        # Erode source image using structuring element created in previous step
-        f1 = binary_erosion(f, nb)
-
-        f2 = f1 - binary_opening(f1,B)
-        y = maximum(y, f2)
-        print y
-    return y
-
-print image
-print mmskelm(image)
+from matplotlib import pyplot as plt
+plt.imshow(distance_transform_edt(image), interpolation='nearest')
+plt.show()
